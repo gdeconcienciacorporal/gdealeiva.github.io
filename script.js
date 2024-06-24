47fdb7926ac28032a8a21419fdd49c6b9b9698d4
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-06-24 05:38:32 -0300

    Add weeks 21 & 22

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //actualizarDiasRestantes();
-  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
-  // Datos para el gráfico de líneas
+//  actualizarDiasRestantes();
+ // setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+   // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 19',
-  data: [12, 12, 13], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 21',
+  data: [17, 12, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 20',
-    data: [13, 10, 26], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 22',
+    data: [19, 13, 22], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
